39df2266c29988f18249d783457d156f67f5423c
Solomon Lambert <sol.lambert@gmail.com>
2023-03-09 16:49:35 -0600

    add more logic for non-authenticated users. display shelf link and do not display form

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ShelfList from '../ShelfList/ShelfList';
 import ShelfForm from '../ShelfForm/ShelfForm';
 
+import { useSelector } from 'react-redux';
+
 function ShelfPage() {
+
+  const user = useSelector(store => store.user)
+
+
   return (
     <div className="container">
       <h2>Shelf</h2>
-      <ShelfForm />
+      {user.id ?
+        <ShelfForm />
+        : ''}
       <ShelfList />
     </div>
   );
